8a587a980340c7f45e3bd039135ec7018c3c04a4
lakshmijayana-dev <lakshmijayana@gmail.com>
2026-03-13 23:12:38 +0530

    Update star_schema.sql

--- part3-datawarehouse/star_schema.sql
+++ part3-datawarehouse/star_schema.sql
@@ -1,1 +1,19 @@
+-- Star Schema Example
 
+CREATE TABLE dim_customer (
+  customer_id INT PRIMARY KEY,
+  name VARCHAR(100),
+  city VARCHAR(50)
+);
+
+CREATE TABLE dim_product (
+  product_id INT PRIMARY KEY,
+  product_name VARCHAR(100)
+);
+
+CREATE TABLE fact_sales (
+  sale_id INT PRIMARY KEY,
+  customer_id INT,
+  product_id INT,
+  amount DECIMAL
+);
